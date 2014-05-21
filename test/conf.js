6a3e0024f4b05c373b23b0692e17f393ea8b208c
Chirayu Krishnappa <chirayu@chirayuk.com>
2014-05-21 16:44:29 -0700

    determine baseUrl and Dartium path from the environment

--- test/conf.js
+++ test/conf.js
@@ -1,5 +1,60 @@
-var baseUrl = (process.env.NGDART_EXAMPLE_BASEURL ||
-               'http://example.ngdart.localhost/');
+/**
+ * Environment Variables affecting this config.
+ * --------------------------------------------
+ *
+ * DARTIUM: The full path to the Dartium binary.
+ *
+ * NGDART_EXAMPLE_BASEURL: Overrides the default baseUrl to one of your
+ *     choosing.  (The default is http://localhost:8080 which is the
+ *     correct if you simply run "pub serve" inside the example folder
+ *     of the AngularDart repo.)
+ */
+
+var env = process.env,
+    fs = require('fs'),
+    path = require('path');
+
+
+function getBaseUrl() {
+  if (env.NGDART_EXAMPLE_BASEURL) {
+    return env.NGDART_EXAMPLE_BASEURL;
+  } else if (env.USER == 'chirayu') {
+    return 'http://example.ngdart.localhost';
+  } else {
+    // Default host:port when you run "pub serve" from the example
+    // subdirectory of the AngularDart repo.
+    return 'http://localhost:8080';
+  }
+}
+
+
+function getDartiumBinary() {
+  var ensure = function(condition) {
+    if (!condition) throw "Unable to locate Dartium.  Please set the DARTIUM environment variable.";
+  };
+
+  // DARTIUM is set by scripts/env.sh for Travis.
+  if (env.DARTIUM) {
+    return env.DARTIUM;
+  }
+  var platform = require('os').platform();
+  var DART_SDK = env.DART_SDK;
+  if (DART_SDK) {
+    // Locate the chromium directory as a sibling of the DART_SDK
+    // directory.  (It's there if you unpacked the full Dart distribution.)
+    var chromiumRoot = path.join(DART_SDK, "../chromium");
+    ensure(fs.existsSync(chromiumRoot));
+    var binary = path.join(chromiumRoot,
+        (platform == 'darwin') ? 'Chromium.app/Contents/MacOS/Chromium' : 'chrome');
+    ensure(fs.existsSync(binary));
+    return binary;
+  }
+  // Last resort: Try the standard location on Macs for the AngularDart team.
+  var binary = '/Applications/dart/chromium/Chromium.app/Contents/MacOS/Chromium';
+  ensure(platform == 'darwin' && fs.existsSync(binary));
+  return binary;
+}
+
 
 exports.config = {
   seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
@@ -13,20 +68,16 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
     'chromeOptions': {
-      'binary': '/Users/chirayu/scripts/dartium',
+      'binary': getDartiumBinary(),
     }
   },
 
-  baseUrl: baseUrl,
+  baseUrl: getBaseUrl(),
 
   jasmineNodeOpts: {
-    // If true, display spec names.
-    isVerbose: true,
-    // If true, print colors to the terminal.
-    showColors: true,
-    // If true, include stack traces in failures.
-    includeStackTrace: true,
-    // Default time to wait in ms before a test fails.
-    defaultTimeoutInterval: 15000
+    isVerbose: true, // display spec names.
+    showColors: true, // print colors to the terminal.
+    includeStackTrace: true, // include stack traces in failures.
+    defaultTimeoutInterval: 15000 // wait time in ms before failing a test.
   },
 };
